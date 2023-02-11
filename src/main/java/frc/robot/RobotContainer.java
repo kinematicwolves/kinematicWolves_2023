c330d7bbdf421204bd186512cda76b92ee554556
Ann McGowan <kinematicwolves@gmail.com>
2023-02-11 14:39:08 -0500

    re-organized robot central

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,9 +1,7 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.DriveRobotOpenLoop;
 import frc.robot.commands.ZeroGyro;
 import frc.robot.subsystems.SwerveSubsytem;
@@ -25,13 +23,14 @@ public class RobotContainer {
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() {;
         // Configure the button bindings
-        configureButtonBindings();
         setDefaultCommands();
     }
 
     public void setDefaultCommands(){
-        s_Swerve.setDefaultCommand(new DriveRobotOpenLoop(s_Swerve, m_driverController));
+        s_Swerve.setDefaultCommand(new DriveRobotOpenLoop(s_Swerve, m_driverController)); //Joysticks
+        s_Swerve.setDefaultCommand(new ZeroGyro(s_Swerve, m_driverController)); // A button
     }
+
 
     /**
      * Use this method to define your button->command mappings. Buttons can be created by
@@ -39,11 +38,7 @@ public class RobotContainer {
      * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
      * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
      */
-    private void configureButtonBindings() {
-        JoystickButton a_driverController = new JoystickButton(m_driverController, XboxController.Button.kA.value);
 
-        a_driverController.whenPressed(new ZeroGyro(s_Swerve));
-    }
 
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
--- src/main/java/frc/robot/commands/ZeroGyro.java
+++ src/main/java/frc/robot/commands/ZeroGyro.java
@@ -4,16 +4,19 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SwerveSubsytem;
 
 public class ZeroGyro extends CommandBase {
   /** Creates a new zeroGyro. */
   private SwerveSubsytem m_swerve;
+  private XboxController m_driverController;
 
-  public ZeroGyro(SwerveSubsytem swerve) {
+  public ZeroGyro(SwerveSubsytem swerve, XboxController driverController) {
     // Use addRequirements() here to declare subsystem dependencies.
     m_swerve = swerve; 
+    m_driverController = driverController;
   }
 
   // Called when the command is initially scheduled.
@@ -23,7 +26,10 @@ public class ZeroGyro extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_swerve.zeroGyro();
+    if (m_driverController.getAButton() == true){
+      m_swerve.zeroGyro();
+    }
+    
   }
 
   // Called once the command ends or is interrupted.
